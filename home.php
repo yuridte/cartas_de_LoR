1bdaaf97d47cb31625149e00442b49805277266c
Yuri Feitosa Duarte <yuridte@gmail.com>
2020-05-08 23:17:43 -0300

    Criando página home e css

---
+++ home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mestres de Runeterra</title>
+    <!-- CSS Styles -->
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/home.css">
+</head>
+<body>
+
+    <div id="background-video">
+        <div class="overlay bg-blue"></div>
+        <video src="video/bilgewater-splashvideo.webm" autoplay loop></video>
+    </div>
+    <div class="container-content">
+        <h1>Teste</h1>
+    </div>
+</body>
+</html>

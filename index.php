ea74652e9d894415c8e71b6409cca30ff79c23f6
Yuri Feitosa Duarte <yuridte@gmail.com>
2020-05-11 16:20:18 -0300

    Ajustando responsivo da página temporária

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html lang="pt">
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-36323975-8"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-36323975-8');
+    </script>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mestres de Runeterra</title>
@@ -21,7 +31,9 @@
 <body>
     <div id="background-video">
         <div class="overlay bg-blue"></div>
-        <video src="video/bilgewater-splashvideo.webm" autoplay loop></video>
+        <video autoplay loop autobuffer muted playsinline>
+            <source src="video/braum-splashvideo.webm" type="video/webm">
+        </video>
     </div>
     <div class="container-content">
         <div id="embreve">
@@ -40,25 +52,23 @@
 
     // CENTRALIZANDO O EM BREVE
     var div = "embreve"; /*Informamos o nome da DIV.*/     
-    
     var alturaTela  = screen.height; /*Atribuimos a variável alturaTela a resolução em Y altura da tela.*/
     var larguraTela = screen.width;  /*Atribuimos a variável larguraTela a resolução em X da tela.*/
-
     var widthDIV  = (document.getElementById(div).offsetWidth / 2) + 73; /*Atribuimos a variável widthDIV a largura da nossa DIV.*/
     var heightDIV = document.getElementById(div).offsetHeight; /*Atribuimos a variável heightDIV a altura da nossa DIV.*/
-
     var posicaoX = (larguraTela / 2) - (widthDIV  / 2) ; /*Explicado logo abaixo.*/
     var posicaoY = (alturaTela  / 2) - (heightDIV / 2); /*A mesma explicação acima para largura se aplica aqui.*/
-    
     document.getElementById(div).style.width  = widthDIV  + "px"; /*Através do Javascript acessamos a propriedade width do CSS e aplicamos a ela o valor em width para nossa DIV, este valor é especificado na variável widthDIV.*/
     document.getElementById(div).style.height = heightDIV + "px"; /*A mesma explicação para widthDIV se aplica aqui, porém aqui é atribuido o valor para altura, heightDIV.*/
-    
     /*Através do document.getElementById() conseguimos acessar um objeto em nosso documento e ter acesso as suas propriedades.*/
-    
     /*No bloco de código abaixo que a mágica acontece, acessamos a propriedade top e left e atribuimos os respectivos valores de nossos cálculos para o posicionamento em tela.*/
-    
     document.getElementById(div).style.top  = posicaoY + "px"; /*Posição da tela referente ao eixo X.*/
     document.getElementById(div).style.left = posicaoX + "px"; /*Posição da tela referente ao eixo Y.*/
+
+    // Identificando posição vertical
+    if(alturaTela > larguraTela){ 
+        document.querySelector('video').classList.add("mobile");
+    }
     </script>
 </body>
 </html>

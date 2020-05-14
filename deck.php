0e880c93360a57daf49001260399a88509dbff47
Yuri Feitosa Duarte <yuridte@gmail.com>
2020-05-14 14:38:05 -0300

    colocando as cartas em arrays separados por units e spell

--- deck.php
+++ deck.php
@@ -9,6 +9,10 @@ if(isset($_GET['id'])){
     $contador = count($deck);
     if ($contador == 1) {
         require_once("header.php"); //cabeçalho do site
+
+        //iniciando variáveis para armazenar as unidades e spell separadamente
+        $units_array = array();
+        $spell_array = array();
         
         foreach ($deck as $deck_info) {
             //extraindo dados do deck
@@ -16,14 +20,28 @@ if(isset($_GET['id'])){
 
             $list_array = explode("|", $list);
             foreach ($list_array as $item) {
+                //separando os dados
                 $item_array = explode("_", $item);
+                ///Vamos procurar as cartas e separar por tipo
+                $cardCode = $item_array[0];
+                $sql_card = "SELECT name, regionRef, type, supertype FROM cards WHERE cardCode LIKE '$cardCode'";
+                $card_array = $dbConn->query($sql_card)->fetchAll();
                 
-                $sql_card = "SELECT * FROM cards"
-                
-                echo $item_array[0] . "<br>";
+                foreach ($card_array as $card_info) {
+                    if ($card_info['type'] == "Unidade") {
+                        array_push($units_array, $card_info);
+                    }elseif ($card_info['type'] == "Feitiço") {
+                        array_push($spell_array, $card_info);
+                    }
+                }
+
             }
     
         }
+
+        //continuar daqui
+        var_dump($units_array[0]);
+        var_dump($spell_array[0]);
     
         require_once("footer.php"); //rodapé do site
     }else{

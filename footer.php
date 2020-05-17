907617d40b4c8cffd40793c5bfbf08b747b129af
Yuri Feitosa Duarte <yuridte@gmail.com>
2020-05-16 22:48:08 -0300

    adicionando algum css/bootstrap

--- footer.php
+++ footer.php
@@ -1,2 +1,10 @@
+<!-- Footer -->
+<footer class="page-footer font-small border-top">
+    <div class="footer-copyright text-center py-3">
+        © 2020 Copyright:
+        <a href="https://mestresderuneterra.com.br"> mestresderuneterra.com.br</a>
+    </div>
+</footer>
+
 </body>
 </html>
--- home.php
+++ home.php
@@ -1,21 +1,98 @@
-<!DOCTYPE html>
-<html lang="pt">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mestres de Runeterra</title>
-    <!-- CSS Styles -->
-    <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/home.css">
-</head>
-<body>
+<?php 
+require_once("header.php"); 
+?>
 
-    <div id="background-video">
-        <div class="overlay bg-blue"></div>
-        <video src="video/bilgewater-splashvideo.webm" autoplay loop></video>
+
+<!-- MENU -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-nav">
+    <a class="navbar-brand" href="/">Mestres de Runeterra</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Dropdown
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+        </ul>
     </div>
-    <div class="container-content">
-        <h1>Teste</h1>
+</nav>
+<!-- FIM menu -->
+
+<div id="background-video">
+    <video autoplay loop autobuffer muted playsinline src="video/bilgewater-splashvideo.webm" id="bg-video"></video>
+</div>
+
+<div id="carouselHome" class="carousel slide banner" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselHome" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselHome" data-slide-to="1"></li>
+    <li data-target="#carouselHome" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1>Seja<br>bem-vindo!</h1>
+                </div>
+                <div class="col">
+                    Seja<br>bem-vindo!
+                </div>
+            </div>
+        </div>
     </div>
-</body>
-</html>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselHome" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselHome" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
+<div class="container-fluid fade-separator"></div>
+
+<div class="container-fluid conteudo-home">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h2>Decks em alta</h2>
+            </div>
+            <div class="col-sm-12">
+                <h2>Decks em alta</h2>
+            </div>
+            <div class="col-sm-12">
+                <h2>Decks em alta</h2>
+            </div>
+            <div class="col-sm-12">
+                <h2>Decks em alta</h2>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+require_once("footer.php");
+?>
